6b050121e7fcc6faa91eaf0b3bf4d16a7c95b5c5
Mohamed Rashad <thegreatrashad@yahoo.com>
2015-12-26 21:05:50 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    
+    
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~ \nWelcome to CMaths (Ultimate C Maths App)\n\n~~~~~~~~~~~~~~~~~~~~~~~~");
+    
+    printf("\n     Would you like to do..?\n\n\t1-check even or odd      2-check prime number or not\n\t3-Check Divisbility      4-Check golden ratio\n\t5-Compute Factorial      6-Compute Fibonacci series\n\t7-Compute Pi             8-Encode Text \n\t9-Decode To text         10-Compute Pascal's Triangle\n\t11-Solve a quadratic equation     12-Logarithms Calculator\n\n");
+    
+    
+
+    
+	return 0;
+}
