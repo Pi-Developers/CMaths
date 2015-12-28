ef7b3cfce378f386c84cc12955b1f93564bebfce
Mohamed Rashad <thegreatrashad@yahoo.com>
2015-12-28 20:03:40 +0200

    0.1 (Still building structure)

--- main.c
+++ main.c
@@ -1,14 +1,126 @@
 #include <stdio.h>
 
-int main(int argc, char **argv){
-    
-    
-	printf("~~~~~~~~~~~~~~~~~~~~~~~~ \nWelcome to CMaths (Ultimate C Maths App)\n\n~~~~~~~~~~~~~~~~~~~~~~~~");
-    
-    printf("\n     Would you like to do..?\n\n\t1-check even or odd      2-check prime number or not\n\t3-Check Divisbility      4-Check golden ratio\n\t5-Compute Factorial      6-Compute Fibonacci series\n\t7-Compute Pi             8-Encode Text \n\t9-Decode To text         10-Compute Pascal's Triangle\n\t11-Solve a quadratic equation     12-Logarithms Calculator\n\n");
-    
-    
+int main(int argc, char** argv){
 
+    int x;
+
+    while(1) {
+
+        printf("\n             ~~~~~~~~~~~~~~~~~~~~~~~~ \n        Welcome to CMaths (Ultimate C Maths App)\n\n             ~~~~~~~~~~~~~~~~~~~~~~~~");
+        printf("\n     Would you like to do..?\n\n\t");
+        printf("1-check even or odd      2-check prime number or not\n\t");
+        printf("3-Check Divisbility      4-Check golden ratio\n\t");
+        printf("5-Compute Factorial      6-Compute Fibonacci series\n\t");
+        printf("7-Compute Pi             8-Encode Text \n\t");
+        printf("9-Decode To text         10-Compute Pascal's Triangle\n\t");
+        printf("11-Solve a quadratic equation     12-Logarithms Calculator\n\n");
+        printf("Choose an option : \n\n>>");
+        
+        scanf("%d", &x);
+
+
+        switch(x) {
+
+        // Check odd or even
+        case 1:
+
+            checkOddOrEven();
+
+            break;
+
+        // check prime number or not
+        case 2:
+
+            checkPrime();
+
+            break;
+
+        // Check Divisbility
+        case 3:
+        
+            checkDivisbility();
+
+            break;
+
+        // Check golden ratio
+        case 4:
+        
+        
+            checkGoldenRatio();
+
+
+            break;
+
+        // Compute Factorial
+        case 5:
+
+
+            break;
+
+        // Compute Fibonacci series
+        case 6:
+
+            break;
+
+        // Compute Pi
+        case 7:
+
+            break;
+
+        // Encode Text
+        case 8:
+
+            break;
+
+        // Decode To text
+        case 9:
+
+            break;
+
+        // Compute Pascal's Triangle
+        case 10:
+
+            break;
+
+        // Solve a quadratic equation
+        case 11:
+
+            break;
+
+        // Logarithms Calculator
+        case 12:
+
+            break;
+        }
+    }
+
+    return 0;
+}
+
+
+/////------------------------------------------------------------------/////
+
+void checkOddOrEven(){
+  
+  
+}
+
+
+void checkPrime(){
+ 
+   
+}
+
+
+void checkDivisbility(){
     
-	return 0;
+    
+}
+
+
+
+void checkGoldenRatio(){
+    
+    
+    
 }
