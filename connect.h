c59ca888ee8d592b21fa9a89adf038700d9756fe
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-01-23 19:47:05 +0200

    V1.0

---
+++ connect.h
@@ -0,0 +1,14 @@
+include "main.c"
+
+void checkOddOrEven();
+void checkPrime();
+void checkDivisbility();
+void checkGoldenRatio();
+void ComputeFactorial();
+void ComputeFibonacci();
+void ComputePi();
+void EncodeText();
+void DecodeTotext();
+void ComputePascal();
+void QuadraticEquation();
+void Logarithm();
